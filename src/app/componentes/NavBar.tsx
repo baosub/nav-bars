bdfe2237dfc5ac00a122a8c18f1abd00290d6d84
Baldo <barrosoub@protonmail.com>
2024-06-15 15:11:14 -0600

    a toggle menu was fixed

--- src/app/componentes/NavBar.tsx
+++ src/app/componentes/NavBar.tsx
@@ -6,41 +6,37 @@ import { Box } from '@mui/material';
 
 
 const pages = [
-    { title: 'Home', path: '/' },
-    { title: 'About', path: '/about'},
-      {title: 'Contact', path: '/contact'},
-     
-  ];
+  { title: 'Home', path: '/' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/contact' },
+
+];
 
 const NavBar = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const toggleMenu = ()=> setOpen(!open);
+  const toggleMenu = () => setOpen(!open);
   return (
     <Box >
-        {open ? <Box sx={{width: '100vw', height: '100vh', bgcolor: 'grey'}} >
+      {open ? (<Box sx={{ width: '100vw', height: '100vh', bgcolor: 'grey' }} >
+        <Box>
 
-            {
-            pages.map((page)=>(
-                <Link onClick={toggleMenu} href={page.path} key={page.title}>{page.title}</Link>
+          <button onClick={toggleMenu}>cerrar</button>
+        </Box>
 
-            ))
-
-        }
-        </Box> : <><button onClick={toggleMenu}>botton para desplegar el menu</button></>}
-
-
-
-        
-        
-      {/*{
-            pages.map((page)=>(
-                <Link href={page.path} key={page.title}>{page.title}</Link>
+        <Box sx={{ width: 1, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <Box>   {
+            pages.map((page) => (
+              <Link onClick={toggleMenu} href={page.path} key={page.title}>{page.title}</Link>
 
             ))
 
-        } */}  
-      
+          }  </Box>
+
+        </Box>
+
+      </Box>) : (<><button onClick={toggleMenu}>botton para desplegar el menu</button></>)}
+
     </Box>
   )
 }
