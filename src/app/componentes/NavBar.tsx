7c2054e3170b63f7b72b2a31f86c3100e1f2549d
Baldo <barrosoub@protonmail.com>
2024-06-15 18:20:44 -0600

    active links solution

--- src/app/componentes/NavBar.tsx
+++ src/app/componentes/NavBar.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import Link from "next/link";
 import { Box } from '@mui/material';
+import { usePathname } from 'next/navigation';
 
 
 const pages = [
@@ -13,6 +14,7 @@ const pages = [
 ];
 
 const NavBar = () => {
+  const pathname = usePathname();
   const [open, setOpen] = useState(false);
 
   const toggleMenu = () => setOpen(!open);
@@ -24,10 +26,31 @@ const NavBar = () => {
           <button onClick={toggleMenu}>cerrar</button>
         </Box>
 
-        <Box sx={{ width: 1, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <Box>   {
+        <Box sx={{ width: 1, height: '100%', display: 'flex',  justifyContent: 'center', alignItems: 'center' }}>
+          <Box sx={{display: 'flex', flexDirection: 'column' }}>   {
             pages.map((page) => (
-              <Link onClick={toggleMenu} href={page.path} key={page.title}>{page.title}</Link>
+              
+                
+                <Box 
+                component={Link} onClick={toggleMenu}
+                href={page.path} key={page.title} passHref
+                
+                
+                sx={{
+                bgcolor: pathname === page.path ? 'red' : 'white',
+                color: pathname === page.path ? 'green': 'pink',
+                marginRight: 2,
+                textDecoration: 'none',
+                '&:hover': {
+                  color: 'yellow',
+                }, textAlign: 'center'
+              }}>
+                {page.title}
+
+                </Box>
+                
+                
+          
 
             ))
 
